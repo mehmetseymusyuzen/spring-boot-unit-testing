3a9fe91791e9e39addf01a5df49948fc810745b4
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-07 00:57:45 +0100

    Refactor compute method in FizzBuzz.java

--- tdd/src/main/java/FizzBuzz.java
+++ tdd/src/main/java/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     // If number divisible by 15, print FizzBuzz
     // If number not divisible by 3 or 5, then print the number
 
-    public String compute(int number) {
+/*    public String compute(int number) {
         if (number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
         else if (number % 3 == 0)
@@ -13,6 +13,21 @@ public class FizzBuzz {
         else if (number % 5 == 0)
             return "Buzz";
         return Integer.toString(number);
+    }*/
+
+    // Refactor compute method
+    public String compute(int number) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (number % 3 == 0)
+            result.append("Fizz");
+        if (number % 5 == 0)
+            result.append("Buzz");
+        if (result.isEmpty())
+            result.append(number);
+
+        return result.toString();
     }
 
 }
